54629d2ac9a42b262f1e05365c933fa6b5771fcf
Igor Imprić <iimpric@gmail.com>
2025-02-18 16:32:17 +0100

    Dodan link na home i readme file

--- obj/Debug/net8.0/FrizerskiSalon_VSITE.AssemblyInfo.cs
+++ obj/Debug/net8.0/FrizerskiSalon_VSITE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrizerskiSalon_VSITE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8089402bcb0a683ca9d7107e0fb87cf24cac1e93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5092cb9d6261606751a5be4bcb13db586bcfa96c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrizerskiSalon_VSITE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrizerskiSalon_VSITE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
